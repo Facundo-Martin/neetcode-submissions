c32ac9b2864c3a3d1e0af44963407abf9491317b
Facundo Martin <facundomartin98@gmail.com>
2026-07-03 15:57:23 -0300

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.py
@@ -0,0 +1,46 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        if len(temperatures) == 1:
+            return [0]
+
+        ans = []
+        n = len(temperatures)
+
+        for i in range(n):
+            # Edge case: The last day never has a warmer day after it
+            if i == n - 1:
+                ans.append(0)
+                break
+                
+            for j in range(i + 1, n):
+                if temperatures[j] > temperatures[i]:
+                    ans.append(j - i)
+                    break
+                elif j == n - 1:
+                    ans.append(0)
+
+        return ans
+
+        
+        # Inputs
+            # Type: Array 1D
+            # Data type: int
+            # Structure: unsorted
+
+
+        # Constraints
+            # 1 <= temperatures.length <= 10^3 
+                # -> O(n^2) won't time out
+                # -> Aim for O(n) time with single pass on array
+            # 1 <= temperatures[i] <= 100
+
+        # Edge cases -> None
+
+        # Patterns
+            # 1. We care about comparing values
+            # 2. We care about latter_temp > former_temp
+
+        # Outputs
+            # Type: Array 1D
+            # Data type: int
+            # Edge cases: No warmer day -> Arr[i] == 0
