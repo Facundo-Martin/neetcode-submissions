dd9b081d136e043df47118ef7af29f37400aff8a
Facundo Martin <facundomartin98@gmail.com>
2026-07-03 15:33:41 -0300

    Add: evaluate-reverse-polish-notation - submission-7

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-7.py
@@ -0,0 +1,45 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        if len(tokens) == 1:
+            return int(tokens[0])
+
+        stack = []
+        for c in tokens:
+                if c == "+":
+                    stack.append(stack.pop() + stack.pop())
+                elif c == "-":
+                    last, first = stack.pop(), stack.pop()
+                    stack.append(first - last)
+                elif c == "*":
+                    stack.append(stack.pop() * stack.pop())
+                elif c == "/":
+                    last, first = stack.pop(), stack.pop()
+                    stack.append(int(first / last))
+                else:
+                    stack.append(int(c))
+
+        return stack.pop()
+
+        # Inputs
+            # tokens:
+                # DS: Array
+                # Data Type: string -> "+", "-", "*", or "/" or -200 <= int <= 200
+                # Structure type: unsorted
+
+        # Constraints & Edge Cases
+            # 1 <= tokens.length <= 1000
+            # tokens[i] is "+", "-", "*", or "/" or -200 <= int <= 200 
+                # Edge case: Dealing with arithmetic while extracting string values! -> Parse
+
+
+        # Keywords & Patterns
+            # 1 <= tokens.length <= 10^3 -> O(n^2) will work (not timeout)
+                # Still aiming for single pass on array (O(n)) + linear time computation O(1)
+            # "valid arithmetic expression" -> Show LIFO pattern -> Leaning toward a stack
+
+
+        # Outputs
+            # Evaluation of the expression
+                # Data type: Integer
+                # constraints? -> None
+
