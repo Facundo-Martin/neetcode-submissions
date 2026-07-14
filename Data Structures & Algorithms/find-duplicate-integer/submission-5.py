a2aa2cf5597702828d6e524e513eba3669c960e5
Facundo Martin <facundomartin98@gmail.com>
2026-07-14 12:46:49 -0300

    Add: find-duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # Phase 1: Finding the intersection point in the cycle
+        tortoise = nums[0]
+        hare = nums[0]
+        
+        # Move once to start the loop
+        tortoise = nums[tortoise]
+        hare = nums[nums[hare]]
+        
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            
+        # Phase 2: Finding the entrance to the cycle (the duplicate)
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+            
+        # return either tortoise or hare, since they are the same now
+        return tortoise
+        
